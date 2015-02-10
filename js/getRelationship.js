388f1414abd9c2242539cfa735e0b63ef9e36554
haqitan <hanqing.tan@gmail.com>
2015-02-10 21:55:18 +1100

    javascript challenge problem - function to is output relationship between 2 numbers

---
+++ js/getRelationship.js
@@ -0,0 +1,24 @@
+function getRelationship(x, y){
+
+  var xNan = isNaN(x);
+  var yNan = isNaN(y);
+
+  if (xNan && yNan) {
+    return "Can't compare relationships because " + x + " and " + y + " are not numbers."
+  }
+
+  if (xNan) {
+    return "Can't compare relationships because " + x + " is not a number."
+  }
+  
+  if (yNan) {
+    return "Can't compare relationships because " + y + " is not a number."
+  }
+
+  if (x < y) { return "<" }
+  if (x > y) { return ">" }
+  if (x == y) { return "=" }
+
+  //console.log("x: " + x );
+  //console.log("y: " + y );
+}
