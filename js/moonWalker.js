174cecc43ad1fbea87471d42c50115121ba30446
haqitan <hanqing.tan@gmail.com>
2015-02-10 23:34:51 +1100

    javascript challenge problem - moonWalker.js

---
+++ js/moonWalker.js
@@ -0,0 +1,14 @@
+var moonWalkers = [
+  "Neil Armstrong",
+  "Buzz Aldrin",
+  "Pete Conrad",
+  "Alan Bean",
+  "Alan Shepard",
+  "Edgar Mitchell",
+  "David Scott",
+  "James Irwin",
+  "John W Young",
+  "Charles Duke",
+  "Eugene Cernan",
+  "Harrison Schmitt"
+];
